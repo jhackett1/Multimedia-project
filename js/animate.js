a122c993e2d21432b7836ae469c28f46bd857cfc
Joshua Hackett <hello@joshuahackett.com>
2016-03-13 20:56:44 +0000

    Nearly done
    Most styling and scripts finished now. Just waiting on content and
    markup.

--- js/animate.js
+++ js/animate.js
@@ -1,16 +1,27 @@
-//For the scroll-down button
-
-$(document).ready(function() {
-
-  $('#scroll-down').hover(function() {
-      $('#prompt').addClass('visible');
-  }, function() {
-      $('#prompt').removeClass('visible');
-  });
-
-});
 
 //For the title animations
+
+
+$(document).ready(function () {
+    var viewport = $(window),
+        setVisible = function (e) {
+            var viewportTop = viewport.scrollTop(),
+                viewportBottom = viewport.scrollTop() + viewport.height();
+            $('.animate').each(function () {
+                var self = $(this),
+                    top = self.offset().top,
+                    bottom = top + self.height(),
+                    topOnScreen = top >= viewportTop && top <= viewportBottom,
+                    bottomOnScreen = bottom >= viewportTop && bottom <= viewportBottom,
+                    elemVisible = topOnScreen || bottomOnScreen;
+                self.toggleClass('fadeInUp', elemVisible);
+                self.toggleClass('animatedDelayed', elemVisible);
+                console.log("Test");
+            });
+        };
+    viewport.scroll(setVisible);
+    setVisible();
+});
 
 
 $(document).ready(function () {
@@ -25,11 +36,22 @@ $(document).ready(function () {
                     topOnScreen = top >= viewportTop && top <= viewportBottom,
                     bottomOnScreen = bottom >= viewportTop && bottom <= viewportBottom,
                     elemVisible = topOnScreen || bottomOnScreen;
-                self.toggleClass('fadeInUp', elemVisible);
-                self.toggleClass('animatedDelayed', elemVisible);
+                self.addClass('fadeInUp', elemVisible);
+                self.addClass('animatedDelayed', elemVisible);
                 console.log("Test");
             });
         };
     viewport.scroll(setVisible);
     setVisible();
+});
+
+
+
+var divs = $('.title');
+$(window).scroll(function(){
+   if($(window).scrollTop()<10){
+         divs.fadeIn("fast");
+   } else {
+         divs.fadeOut("fast");
+   }
 });
